9f34bea9ae0181ebd9cc674bc887ad6b1487266e
Drew Faulk <drew.faulk@quincycompressor.com>
2018-09-04 09:51:05 -0500

    Changed component name and added methods and data to main component.

---
+++ client/src/components/Calculator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  props: {
+  },
+  data: function(){
+    return {
+      msg: "Calculator web app."
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
