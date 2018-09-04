147337aead7397e0cda5ed9ec4b8c0934ba43efe
Drew Faulk <drew.faulk@quincycompressor.com>
2018-09-04 15:52:08 -0500

    More work done on KWToAmps Component

--- client/src/components/AmpsToKW.vue
+++ client/src/components/AmpsToKW.vue
@@ -15,7 +15,7 @@
 
 <script>
 export default {
-  name: 'AmsToKW',
+  name: 'AmpsToKW',
   data () {
     return {
       selected: null,
--- client/src/components/KWToAmps.vue
+++ client/src/components/KWToAmps.vue
@@ -5,6 +5,60 @@
         <h5>{{ msg }}</h5>
       </b-col>
     </b-row>
+    <b-row align-h="center">
+      <b-col class="text-right" cols="2">
+        <label>Current Type: </label>
+      </b-col>
+      <b-col cols="6">
+        <b-form-select v-model="selected" :options="options"/>
+      </b-col>
+    </b-row>
+    <b-row class="mt-2" align-h="center">
+      <b-col class="text-right" cols="2">
+        <label>Power in kW: </label>
+      </b-col>
+      <b-col cols="5">
+        <b-form-input v-model="kW" :state="currentStatekW" type="number"></b-form-input>
+      </b-col>
+      <b-col class="text-left" cols="1">
+        <label class="kW-lbl">kW</label>
+      </b-col>
+    </b-row>
+    <b-row v-if="selected === 'c'" class="mt-2" align-h="center">
+      <b-col class="text-right" cols="2">
+        <label>Voltage Type: </label>
+      </b-col>
+      <b-col cols="6">
+        <b-form-select v-model="selected_volt" :options="options_volt"/>
+      </b-col>
+    </b-row>
+    <b-row class="mt-2" align-h="center">
+      <b-col class="text-right" cols="2">
+        <label>Voltage in volts: </label>
+      </b-col>
+      <b-col cols="5">
+        <b-form-input v-model="volts" :state="currentStatevolts" type="number"></b-form-input>
+      </b-col>
+      <b-col class="text-left" cols="1">
+        <label>V</label>
+      </b-col>
+    </b-row>
+    <b-row v-if="selected !== 'a'" class="mt-2" align-h="center">
+      <b-col class="text-right" cols="2">
+        <label>Power factor: </label>
+      </b-col>
+      <b-col cols="5">
+        <b-form-input v-model="pf" :state="currentStatepf" type="number"></b-form-input>
+      </b-col>
+      <b-col class="text-left" cols="1">
+        <label></label>
+      </b-col>
+    </b-row>
+    <b-row class="mt-2 text-center">
+      <b-col>
+        <b-button variant="outline-primary">Calculate</b-button>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -13,10 +67,37 @@ export default {
   name: 'KWToAmps',
   data () {
     return {
-      msg: 'kW to Amps'
+      msg: 'kW to Amps',
+      selected: null,
+      selected_volt: null,
+      options: [
+        { value: null, text: 'Please select an option' },
+        { value: 'a', text: 'DC' },
+        { value: 'b', text: 'AC - Single Phase' },
+        { value: 'c', text: 'AC - Three Phase'}
+      ],
+      options_volt: [
+        { value: null, text: 'Please select an option' },
+        { value: 'a', text: 'Line to line voltage' },
+        { value: 'b', text: 'Line to neutral voltage' }
+      ],
+      kW: '',
+      volts: '',
+      pf: ''
     }
   },
   methods: {
+  },
+  computed: {
+    currentStatekW () {
+      return this.kW >= 0 ? true : false 
+    },
+    currentStatevolts () {
+      return this.volts >= 0 ? true : false
+    },
+    currentStatepf () {
+      return this.pf >= 0 ? true : false
+    }
   }
 }
 </script>
